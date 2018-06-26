e440d671a3bb863aa800894e6cfa9d21520c1c47
adeojoemmanuel <emmanuel.adeojo@yahoo.com>
2018-06-26 07:57:40 +0100

    config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+  /**
+   * Contains all configurations
+   *
+   */
+   return [
+      'dbname' => 'dubexstore',
+      'pass' => '',
+      'username' => 'root',
+      'host' => 'localhost',
+    ];
+
+?>
